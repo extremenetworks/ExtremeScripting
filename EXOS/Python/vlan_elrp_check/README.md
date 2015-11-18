7b912a4b42fad46eb4b659fa8a7865e8319bad19
stewilliams-extr <stewilliams@extremenetworks.com>
2015-11-18 10:58:38 -0500

    Added VLAN ELRP check script.
    This script will run ELRP on all VLANS on an EXOS switch.

---
+++ EXOS/Python/vlan_elrp_check/README.md
@@ -0,0 +1,131 @@
+# VLAN ELRP check
+
+## Description
+This Script will run ELRP on all VLANS on an EXOS switch.  If ELRP is disabled the script will enable it, run ELRP on all vlans and then disable it back.
+
+
+### Files
+
+* [vlan_elrp_check.py](vlan_elrp_check.py) 	-  The Core Python Script
+* [README.md](README.md)
+
+
+### Requirements
+* Firmware: ExtremeXOS(TM) 15.6
+* This script was tested on 15.6 and older.
+
+### Features
+* This Scrip will run ELRP on all created vlans.
+* If more than 20 vlans Have been created the script will check to make sure you want to run ELRP on all VLANS as it can take a while. 
+* If elrp is disabled the script will temporaly enable ELRP during the test and disable it after the script is done.
+ 
+
+### How to use
+* Run the script on a switch with EXOS 15.6 and higher.
+
+##### Switch script example
+```
+Switch# run script vlan_elrp_check.py
+
+
+*************************************
+* ELRP has been temporarily Enabled *
+*************************************
+
+
+Running ELRP on VLAN Default.
+
+Starting ELRP Poll . . .
+# NO LOOP DETECTED # --- vlan "Default" elrp statistics ---
+3 packets transmitted, 0 received, ingress port (nil)
+
+
+
+Running ELRP on VLAN ISC_MLAG.
+
+Starting ELRP Poll . . .
+# NO LOOP DETECTED # --- vlan "ISC_MLAG" elrp statistics ---
+3 packets transmitted, 0 received, ingress port (nil)
+
+
+
+Running ELRP on VLAN Mgmt.
+
+Starting ELRP Poll . . .
+# NO LOOP DETECTED # --- vlan "Mgmt" elrp statistics ---
+3 packets transmitted, 0 received, ingress port (nil)
+
+
+
+Running ELRP on VLAN V10_C0.
+
+Starting ELRP Poll . . .
+# NO LOOP DETECTED # --- vlan "V10_C0" elrp statistics ---
+3 packets transmitted, 0 received, ingress port (nil)
+
+
+
+Running ELRP on VLAN V11_C1.
+
+Starting ELRP Poll . . .
+# NO LOOP DETECTED # --- vlan "V11_C1" elrp statistics ---
+3 packets transmitted, 0 received, ingress port (nil)
+
+
+
+Running ELRP on VLAN V12_C2.
+
+Starting ELRP Poll . . .
+# NO LOOP DETECTED # --- vlan "V12_C2" elrp statistics ---
+3 packets transmitted, 0 received, ingress port (nil)
+
+
+
+Running ELRP on VLAN v13_C2_1.
+
+Starting ELRP Poll . . .
+# NO LOOP DETECTED # --- vlan "v13_C2_1" elrp statistics ---
+3 packets transmitted, 0 received, ingress port (nil)
+
+
+
+**************
+*Cleaning up!*
+**************
+Ending status of ELRP is: Disabled
+Switch# 
+```
+
+## License
+Copyright© 2015, Extreme Networks
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## Support
+The software is provided as is and Extreme Networks has no obligation to provide
+maintenance, support, updates, enhancements or modifications.
+Any support provided by Extreme Networks is at its sole discretion.
+
+Issues and/or bug fixes may be reported on [The Hub](https://community.extremenetworks.com/extreme).
+
+>Be Extreme
---
+++ EXOS/Python/vlan_elrp_check/vlan_elrp_check.py
@@ -0,0 +1,123 @@
+'''
+This Python Script will run elrp on every configured VLAN on the switch.  If you have > 20 of VLANs this script will ask if you want to run as it can take a long time
+Please use caution when running this script.
+
+'''
+#This function is is used to break cli output into a list of dictionary for easy parsing.
+def cmd2data(clicmd):
+    import re
+    import xml.etree.cElementTree as ElementTree
+    re_reply = re.compile(r'<reply>.+?</reply>', re.DOTALL)
+    xmlout = exsh.clicmd(clicmd, capture=False, xml=True)
+    data = []
+    for reply in re.finditer(re_reply, xmlout):
+        if reply:
+            reply_xml = reply.group()
+            root = ElementTree.fromstring(reply_xml)
+            for message in root.iter('message'):
+                for element in message:
+                    mdata = {}
+                    edata = {}
+                    for e in element:
+                        text = int(e.text) if e.text is not None and e.text.isdigit() else e.text
+                        edata[e.tag] = text
+                    mdata[element.tag] = edata
+                    data.append(mdata)
+    return data
+
+#this is the main function of the script.  It checks to see if there are more that 20 vlans configured.
+#It also will call other functions to start the elrp script
+def start_elrp_script():
+    answer = ("")
+    if  len(get_vlans()) > 20:
+        print ("Are you sure you want to run elrp on %d VLAN's?") % (len(get_vlans()))
+        answer = raw_input("y/n: ").lower()
+
+        if answer == 'y':
+            print("continuing")
+            Run_ELRP()
+
+        elif answer == 'n':
+            print
+            print ("Script Canceled, ending elrp status is: %s") % (check_elrp_stat())
+            print
+            print
+        elif answer != 'y' and answer != 'n':
+            print
+            print ("Please select 'y' for Yes and 'n' for No")
+            print
+            print
+            start_elrp_script()
+    else:
+        Run_ELRP()
+
+#defining a function to check if ELRP is Enabled or not
+def check_elrp_stat():
+    elrp_status = cmd2data('show elrp')
+    elrp_status =  elrp_status[0]['elrpGlobalCfg']['enable']
+    if elrp_status == 1:
+        return 'Enabled'
+    else:
+        return 'Disabled'
+
+#defining function that gets all vlans and places them into a table
+def get_vlans():
+    ##Getting the data needed for vlans and creating a blank table to put them.
+    show = cmd2data('show vlan')
+    number_of_lines = len(show)
+    vlan_table = []
+
+    #scraping output for vlan's and appending them to the "vlan_table"
+    for line in show:
+        vlan_name = line['vlanProc']['name1']
+        vlan_table.append(vlan_name)
+    return vlan_table
+
+#Runs ELRP on all vlans found in get_vlans function
+def Run_ELRP():
+    #saving the start value of elrp
+    elrp_start_value = check_elrp_stat()
+
+    #Making sure ELRP is Enabled
+    if check_elrp_stat() == 'Enabled':
+        print
+        print
+        print ("**************************")
+        print ("* ELRP is already Enabled*")
+        print ("**************************")
+        print
+        print
+    else:
+        (exsh.clicmd('enable elrp', True))
+        print
+        print
+        print ("*************************************")
+        print ("* ELRP has been temporarily Enabled *")
+        print ("*************************************")
+        print
+        print
+
+    #Running ELRP using the get_vlans function to get a table list of vlans
+    for vlan in get_vlans():
+        print ("Running ELRP on VLAN %s.") % (vlan)
+        elrp_output = (exsh.clicmd('configure elrp-client one-shot %s ports all print' % (vlan), True))
+        print elrp_output
+        print
+        print
+
+
+    #print elrp_status and disable ELRP if needed
+
+    if elrp_start_value == 'Enabled':
+        print ("********************")
+        print ("*No clean up needed*")
+        print ("********************")
+        print ("Ending status of ELRP is: %s") % check_elrp_stat()
+    else:
+        print ("**************")
+        print ("*Cleaning up!*")
+        print ("**************")
+        (exsh.clicmd('disable elrp', True))
+        print ("Ending status of ELRP is: %s") % check_elrp_stat()
+
+start_elrp_script()
