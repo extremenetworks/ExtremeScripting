675799f2df87f2298956a56a977c5dcd6e0a624a
stewilliams-extr <stewilliams@extremenetworks.com>
2016-02-10 08:54:26 -0500

    Update mlag_config_check.py
    Added support if MLAG peer is using a user created VR. 
    Added print statement showing the peer that the check was run on. 
    Added error out when MLAG peer doe not have a ISC vlan

--- EXOS/Python/mlag_config_check/mlag_config_check.py
+++ EXOS/Python/mlag_config_check/mlag_config_check.py
@@ -4,158 +4,126 @@
 # that the tagging on vlans matches across MLAG peers.
 # 
 # Last updated: June 29, 2015
+from sys import exit
 
 def main():
-	# Find which vlan is the ISC vlan
-	cli_output = exsh.clicmd('debug vsm show peer | inc "vrId      : 2"', True).split()
-	isc_vlan = cli_output[5]
+    # Find which vlan is the ISC vlan
+    cli_output = exsh.clicmd('debug vsm show peer | inc "vrId      :"', True).split()
+    isc_vlan = cli_output[5]
+    peer_name = exsh.clicmd('debug vsm show peer | include "Name      : "', True).split()
 
-	# Check the number of ports added to the ISC vlan, and get the port number of the ISC link
-	cli_output = exsh.clicmd('show vlan ' + isc_vlan, True).split()
+    #Checkes to see if MLAG peer is not configured
+    if cli_output[5] == 'Unknown':
+        print ("Tool does not support this configuration")
+        exit()        
+    else:
+        print
+        print ("****mlag_config_check.py running on MLAG Peer {0}****").format(peer_name[6])
+        print  # Check the number of ports added to the ISC vlan, and get the port number of the ISC link
+        cli_output = exsh.clicmd('show vlan ' + isc_vlan, True).split()
+        index = cli_output.index('Ports:')
+        index = index + 1 # +1, since we found the item before the number of ports
+        num_ports = cli_output[index]
+        # Remove the trailing period from that output
+        if num_ports.endswith('.'):
+            num_ports = int(num_ports[:-1])
+        #Check the number of ports added to the ISC vlan
+        if num_ports > 1:            
+            print 'Multiple ports are added to the ISC vlan (' + isc_vlan + '). Please correct.'
+            return
+        elif num_ports == 0:
+            print 'No ports are added to the ISC vlan (' + isc_vlan + '). Please correct.'
+            return
+        # At this point, we can assume that there is one port added to the ISC vlan,
+        # and continue checking the MLAG configuration.
+        # Determine what the port number of the ISC is
+        if 'Tag:' in cli_output:
+            index = cli_output.index('Tag:')
+            index = index + 1
+            isc_port = cli_output[index]
+        elif 'Untag:' in cli_output:
+            index = cli_output.index('Untag:')
+            index = index + 1
+            isc_port = cli_output[index]
+        else:
+            print 'Error: An error occurred. Unable to determine ISC port'
+            return
+        # Remove the flags around the port number
+        if isc_port.endswith('g'):
+            isc_port = isc_port[:-1]
+        elif isc_port.endswith('G'):
+            print "The ISC port is also configured as an MLAG port. Please correct."
+            return
+        if isc_port.startswith('*'):
+            isc_port = isc_port[1:]
+        elif isc_port.startswith('!'):
+            isc_port = isc_port[1:]
+            print 'The ISC port (' + isc_port + ') is disabled. Please correct.'
+        # Create a list of the vlans that exist on the ISC port
+        cli_output = exsh.clicmd('show port ' + isc_port + ' info detail', True).split()
+        vlan_on_next_iteration = False
+        isc_port_vlans = list()
+        for i in cli_output:
+            if vlan_on_next_iteration:
+                #remove a trailing comma, if present
+                if i.endswith(','):
+                    vlan = i[:-1]
+                else:
+                    vlan = i
+                isc_port_vlans.append(vlan)
+            if i == 'Name:':
+                vlan_on_next_iteration = True
+            else :
+                vlan_on_next_iteration = False
+        # Now, isc_port_vlans contains all the vlans on the ISC port.
+        # Now we can iterate through MLAG ports, and check if the vlans on them are
+        # on the ISC port. If not, an error will be written to the CLI.
+        cli_output = exsh.clicmd('show config vsm', True)
+        lines = cli_output.split('\n')
+        #remove an empty string that breaks the for loop below
+        lines = lines[:-1]
+        #create an empty list to be used in the loop
+        mlag_ports = list()
+        for l in lines:
+            line = l.split()
+            if line[0] == 'enable':
+                mlag_ports.append(line[3])
+        for p in mlag_ports:
+            cli_output = exsh.clicmd('show port ' + p + ' info detail', True).split()
+            vlan_on_next_iteration = False
+            port_vlans = list()
+            for i in cli_output:
+                if vlan_on_next_iteration:
+                    #remove a trailing comma, if present
+                    if i.endswith(','):
+                        vlan = i[:-1]
+                    else:
+                        vlan = i
+                    if vlan not in isc_port_vlans:
+                        print 'Vlan ' + vlan + ' is not added to the ISC port (' + isc_port + '), and is found on MLAG port ' + p + '. Please correct.'
+                if i == 'Name:':
+                    vlan_on_next_iteration = True
+                else :
+                    vlan_on_next_iteration = False
+        ## Check local and remote checksums to determine if FDB and VLANs match
+        for count in range(0,3):
+            checksums = exsh.clicmd('debug fdb show globals | include LclCkhsum:', True)
+            checksums = checksums.split()
+            local = checksums[2]
+            remote = checksums[4]
+            #print local
+            #print remote
+            if local == remote:
+                print ('Local and remote FDB checksums match.')
+                break
+        if local != remote:
+            print 'Local and remote FDB checksums do not match. Please check config on the other MLAG peer.'
+            print exsh.clicmd('debug fdb show globals', True)
+        print 'MLAG config check completed.'
 
-	index = cli_output.index('Ports:')
-	index = index + 1 # +1, since we found the item before the number of ports
-
-	num_ports = cli_output[index]
-
-	# Remove the trailing period from that output
-
-	if num_ports.endswith('.'):
-		num_ports = int(num_ports[:-1])
-
-	#Check the number of ports added to the ISC vlan
-
-	if num_ports > 1:
-		print 'Multiple ports are added to the ISC vlan (' + isc_vlan + '). Please correct.'
-		return
-	elif num_ports == 0:
-		print 'No ports are added to the ISC vlan (' + isc_vlan + '). Please correct.'
-		return
-
-	# At this point, we can assume that there is one port added to the ISC vlan,
-	# and continue checking the MLAG configuration.
-
-	# Determine what the port number of the ISC is
-
-	if 'Tag:' in cli_output:
-		index = cli_output.index('Tag:')
-		index = index + 1
-		isc_port = cli_output[index]
-	elif 'Untag:' in cli_output:
-		index = cli_output.index('Untag:')
-		index = index + 1
-		isc_port = cli_output[index]
-	else:
-		print 'Error: An error occurred. Unable to determine ISC port'
-		return
-
-	# Remove the flags around the port number
-
-	if isc_port.endswith('g'):
-		isc_port = isc_port[:-1]
-	elif isc_port.endswith('G'):
-		print "The ISC port is also configured as an MLAG port. Please correct."
-		return
-
-	if isc_port.startswith('*'):
-		isc_port = isc_port[1:]
-	elif isc_port.startswith('!'):
-		isc_port = isc_port[1:]
-		print 'The ISC port (' + isc_port + ') is disabled. Please correct.'
-
-	# Create a list of the vlans that exist on the ISC port
-
-	cli_output = exsh.clicmd('show port ' + isc_port + ' info detail', True).split()
-
-	vlan_on_next_iteration = False
-
-	isc_port_vlans = list()
-
-	for i in cli_output:
-		
-		if vlan_on_next_iteration:
-			#remove a trailing comma, if present
-			if i.endswith(','):
-				vlan = i[:-1]
-			else:
-				vlan = i
-			isc_port_vlans.append(vlan)
-
-			
-
-
-		if i == 'Name:':
-			vlan_on_next_iteration = True
-		else :
-			vlan_on_next_iteration = False
-
-	# Now, isc_port_vlans contains all the vlans on the ISC port.
-	# Now we can iterate through MLAG ports, and check if the vlans on them are
-	# on the ISC port. If not, an error will be written to the CLI.
-
-	cli_output = exsh.clicmd('show config vsm', True)
-
-	lines = cli_output.split('\n')
-
-	#remove an empty string that breaks the for loop below
-
-	lines = lines[:-1]
-
-	#create an empty list to be used in the loop
-
-	mlag_ports = list()
-
-	for l in lines:
-		line = l.split()
-		if line[0] == 'enable':
-			mlag_ports.append(line[3])
-
-
-	for p in mlag_ports:
-
-
-		cli_output = exsh.clicmd('show port ' + p + ' info detail', True).split()
-
-		vlan_on_next_iteration = False
-
-		port_vlans = list()
-
-		for i in cli_output:
-			
-			if vlan_on_next_iteration:
-				#remove a trailing comma, if present
-				if i.endswith(','):
-					vlan = i[:-1]
-				else:
-					vlan = i
-				if vlan not in isc_port_vlans:
-					print 'Vlan ' + vlan + ' is not added to the ISC port (' + isc_port + '), and is found on MLAG port ' + p + '. Please correct.'
-
-			if i == 'Name:':
-				vlan_on_next_iteration = True
-			else :
-				vlan_on_next_iteration = False
-
-	## Check local and remote checksums to determine if FDB and VLANs match
-	for count in range(0,3):
-		checksums = exsh.clicmd('debug fdb show globals | include LclCkhsum:', True)
-
-		checksums = checksums.split()
-    
-		local = checksums[2]
-		remote = checksums[4]
-
-		#print local
-		#print remote
-    
-		if local == remote:
-			print ('Local and remote FDB checksums match.')
-			break
-	if local != remote:
-		print 'Local and remote FDB checksums do not match. Please check config on the other MLAG peer.'
-		print exsh.clicmd('debug fdb show globals', True)
-    
-    
-	print 'MLAG config check completed.'
-
-main()
+if __name__ == '__main__':
+    try:
+        main()
+    except SystemExit:
+        # catch SystemExit to prevent EXOS shell from exiting to the login prompt
+        pass
