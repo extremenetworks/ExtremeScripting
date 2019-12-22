8596da10721f83629e18d97475af649d6f2f314e
Robert Weiler <18140711+rbrt-weiler@users.noreply.github.com>
2019-12-22 09:16:17 +0100

    Bump GenericNbiClient.go to v0.8.0 (#58)
    * Bumped GenericNbiClient.go to v0.8.0.
    
    * Switched to relative paths in various READMEs.

--- Netsight/nbi_clients/GenericNbiClient.go/GenericNbiClient.go
+++ Netsight/nbi_clients/GenericNbiClient.go/GenericNbiClient.go
@@ -41,9 +41,11 @@ import (
 
 // AppConfig stores the application configuration once parsed by flags.
 type AppConfig struct {
+	accessScheme    string
 	XMCHost         string
 	XMCPort         uint
 	HTTPTimeout     uint
+	NoHTTPS         bool
 	InsecureHTTPS   bool
 	XMCClientID     string
 	XMCClientSecret string
@@ -63,23 +65,35 @@ type OAuth2Token struct {
 
 // OAuth2TokenElements stores decoded information contained in a valid OAuth2 token.
 type OAuth2TokenElements struct {
-	Issuer           string   `json:"iss,omitempty"`
-	Subject          string   `json:"sub,omitempty"`
-	JWTID            string   `json:"jti,omitempty"`
-	Roles            []string `json:"roles,omitempty"`
-	IsuedAtUnixfmt   int64    `json:"iat,omitempty"`
-	NotBeforeUnixfmt int64    `json:"nbf,omitempty"`
-	ExpiresAtUnixfmt int64    `json:"exp,omitempty"`
-	IssuedAt         time.Time
-	NotBefore        time.Time
-	ExpiresAt        time.Time
-	LongLived        bool `json:"longLived,omitempty"`
+	Header    OAuth2HeaderElements
+	Payload   OAuth2PayloadElements
+	Signature []byte
+}
+
+// OAuth2HeaderElements stores decoded information contained in the header part of an OAuth2 token.
+type OAuth2HeaderElements struct {
+	Algorithm string `json:"alg"`
+}
+
+// OAuth2PayloadElements stores decoded information contained in the payload part of an OAuth2 token.
+type OAuth2PayloadElements struct {
+	Issuer           string    `json:"iss,omitempty"`
+	Subject          string    `json:"sub,omitempty"`
+	JWTID            string    `json:"jti,omitempty"`
+	Roles            []string  `json:"roles,omitempty"`
+	IsuedAtUnixfmt   int64     `json:"iat,omitempty"`
+	NotBeforeUnixfmt int64     `json:"nbf,omitempty"`
+	ExpiresAtUnixfmt int64     `json:"exp,omitempty"`
+	IssuedAt         time.Time `json:"-"`
+	NotBefore        time.Time `json:"-"`
+	ExpiresAt        time.Time `json:"-"`
+	LongLived        bool      `json:"longLived,omitempty"`
 }
 
 // Definitions used within the code.
 const (
 	toolName      string = "XMC NBI GenericNbiClient.go"
-	toolVersion   string = "0.6.6"
+	toolVersion   string = "0.8.0"
 	httpUserAgent string = toolName + "/" + toolVersion
 	jsonMimeType  string = "application/json"
 )
@@ -134,13 +148,15 @@ func getEnvOrDefaultBool(name string, defaultVal bool) bool {
 }
 
 // retrieveOAuthToken retrieves a usable OAuth token from XMC.
-func retrieveOAuthToken() (string, int, error) {
-	tokenURL := "https://" + Config.XMCHost + ":" + fmt.Sprint(Config.XMCPort) + "/oauth/token/access-token?grant_type=client_credentials"
+func retrieveOAuthToken() (OAuth2Token, error) {
+	var tokenData OAuth2Token
+
+	tokenURL := Config.accessScheme + "://" + Config.XMCHost + ":" + fmt.Sprint(Config.XMCPort) + "/oauth/token/access-token?grant_type=client_credentials"
 
 	// Generate an actual HTTP request.
 	req, reqErr := http.NewRequest(http.MethodPost, tokenURL, nil)
 	if reqErr != nil {
-		return "", errHTTPRequest, fmt.Errorf("Could not create HTTPS request: %s", reqErr)
+		return tokenData, fmt.Errorf("Could not create HTTPS request: %s", reqErr)
 	}
 	req.Header.Set("User-Agent", httpUserAgent)
 	req.Header.Set("Cache-Control", "no-cache")
@@ -151,60 +167,105 @@ func retrieveOAuthToken() (string, int, error) {
 	// Try to get a result from the API.
 	res, resErr := NBIClient.Do(req)
 	if resErr != nil {
-		return "", errXMCConnect, fmt.Errorf("Could not connect to XMC: %s", resErr)
+		return tokenData, fmt.Errorf("Could not connect to XMC: %s", resErr)
 	}
 	if res.StatusCode != 200 {
-		return "", errXMCConnect, fmt.Errorf("Got status code %d instead of 200", res.StatusCode)
+		return tokenData, fmt.Errorf("Got status code %d instead of 200", res.StatusCode)
 	}
 	defer res.Body.Close()
 
 	// Check if the HTTP response has yielded the expected content type.
 	resContentType := res.Header.Get("Content-Type")
 	if strings.Index(resContentType, jsonMimeType) != 0 {
-		return "", errHTTPResponse, fmt.Errorf("Content-Type %s returned instead of %s", resContentType, jsonMimeType)
+		return tokenData, fmt.Errorf("Content-Type %s returned instead of %s", resContentType, jsonMimeType)
 	}
 
 	// Read and parse the body of the HTTP response.
 	xmcToken, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
-		return "", errHTTPResponse, fmt.Errorf("Could not read server response: %s", readErr)
+		return tokenData, fmt.Errorf("Could not read server response: %s", readErr)
 	}
-	jsonErr := json.Unmarshal(xmcToken, &OAuth)
+	jsonErr := json.Unmarshal(xmcToken, &tokenData)
 	if jsonErr != nil {
-		return "", errHTTPResponse, fmt.Errorf("Could not read server response: %s", jsonErr)
+		return tokenData, fmt.Errorf("Could not read server response: %s", jsonErr)
 	}
 
-	return OAuth.AccessToken, errSuccess, nil
+	return tokenData, nil
 }
 
-// decodeOAuthToken decodes certain parts of the OAuth token.
-func decodeOAuthToken() (OAuth2TokenElements, error) {
+// decodeOAuthToken decodes the JSON Web Token used for OAuth.
+func decodeOAuthToken(accessToken string) (OAuth2TokenElements, error) {
 	var tokenElements OAuth2TokenElements
 
-	// Seperate and base64 decode the relevant part of the token.
-	tokenParts := strings.Split(OAuth.AccessToken, ".")
-	tokenData, tokenErr := base64.StdEncoding.DecodeString(fmt.Sprintf("%s==", tokenParts[1]))
-	if tokenErr != nil {
-		return tokenElements, tokenErr
-	}
+	// Seperate the parts of the token.
+	tokenParts := strings.Split(accessToken, ".")
 
-	// Parse the JSON into our struct.
-	decodeErr := json.Unmarshal(tokenData, &tokenElements)
-	if decodeErr != nil {
-		return tokenElements, decodeErr
+	// Decode each part.
+	header, headerErr := decodeOAuthHeader(tokenParts[0])
+	if headerErr != nil {
+		return tokenElements, headerErr
 	}
-
-	// Transform UNIX timestamps into time.Time objects.
-	tokenElements.IssuedAt = time.Unix(tokenElements.IsuedAtUnixfmt, 0)
-	tokenElements.NotBefore = time.Unix(tokenElements.NotBeforeUnixfmt, 0)
-	tokenElements.ExpiresAt = time.Unix(tokenElements.ExpiresAtUnixfmt, 0)
+	tokenElements.Header = header
+	payload, payloadErr := decodeOAuthPayload(tokenParts[1])
+	if payloadErr != nil {
+		return tokenElements, payloadErr
+	}
+	tokenElements.Payload = payload
+	signature, signatureErr := base64.RawURLEncoding.DecodeString(tokenParts[2])
+	if signatureErr != nil {
+		return tokenElements, signatureErr
+	}
+	tokenElements.Signature = signature
 
 	return tokenElements, nil
 }
 
+// decodeOAuthPayload decodes the header part of the JSON Web Token used for OAuth
+func decodeOAuthHeader(header64 string) (OAuth2HeaderElements, error) {
+	var headerElements OAuth2HeaderElements
+
+	// Decode the base64url encoded JSON.
+	headerData, headerErr := base64.RawURLEncoding.DecodeString(header64)
+	if headerErr != nil {
+		return headerElements, headerErr
+	}
+
+	// Parse the JSON into our struct.
+	decodeErr := json.Unmarshal(headerData, &headerElements)
+	if decodeErr != nil {
+		return headerElements, decodeErr
+	}
+
+	return headerElements, nil
+}
+
+// decodeOAuthPayload decodes the payload part of the JSON Web Token used for OAuth
+func decodeOAuthPayload(payload64 string) (OAuth2PayloadElements, error) {
+	var payloadElements OAuth2PayloadElements
+
+	// Decode the base64url encoded JSON.
+	payloadData, payloadErr := base64.RawURLEncoding.DecodeString(payload64)
+	if payloadErr != nil {
+		return payloadElements, payloadErr
+	}
+
+	// Parse the JSON into our struct.
+	decodeErr := json.Unmarshal(payloadData, &payloadElements)
+	if decodeErr != nil {
+		return payloadElements, decodeErr
+	}
+
+	// Transform UNIX timestamps into time.Time objects.
+	payloadElements.IssuedAt = time.Unix(payloadElements.IsuedAtUnixfmt, 0)
+	payloadElements.NotBefore = time.Unix(payloadElements.NotBeforeUnixfmt, 0)
+	payloadElements.ExpiresAt = time.Unix(payloadElements.ExpiresAtUnixfmt, 0)
+
+	return payloadElements, nil
+}
+
 // retrieveAPIResult sends the given query to XMC and returns the raw JSON result, an error code for os.Exit() and the actual error.
 func retrieveAPIResult(query string) (string, int, error) {
-	apiURL := "https://" + Config.XMCHost + ":" + fmt.Sprint(Config.XMCPort) + "/nbi/graphql"
+	apiURL := Config.accessScheme + "://" + Config.XMCHost + ":" + fmt.Sprint(Config.XMCPort) + "/nbi/graphql"
 
 	// Generate an actual HTTP request.
 	jsonQuery, jsonQueryErr := json.Marshal(map[string]string{"query": query})
@@ -220,7 +281,7 @@ func retrieveAPIResult(query string) (string, int, error) {
 	req.Header.Set("Content-Type", jsonMimeType)
 	req.Header.Set("Accept", jsonMimeType)
 	if Config.UseOAuth {
-		req.Header.Set("Authorization", "Bearer "+OAuth.AccessToken)
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", OAuth.AccessToken))
 	} else {
 		req.SetBasicAuth(Config.XMCUsername, Config.XMCPassword)
 	}
@@ -250,10 +311,12 @@ func retrieveAPIResult(query string) (string, int, error) {
 	return string(body), errSuccess, nil
 }
 
+// parseCLIOptions parses all options passed by env or CLI into the Config variable.
 func parseCLIOptions() {
 	flag.StringVar(&Config.XMCHost, "host", getEnvOrDefaultString("XMCHOST", ""), "XMC Hostname / IP")
 	flag.UintVar(&Config.XMCPort, "port", getEnvOrDefaultUint("XMCPORT", 8443), "HTTP port where XMC is listening")
 	flag.UintVar(&Config.HTTPTimeout, "httptimeout", getEnvOrDefaultUint("XMCTIMEOUT", 5), "Timeout for HTTP(S) connections")
+	flag.BoolVar(&Config.NoHTTPS, "nohttps", getEnvOrDefaultBool("XMCNOHTTPS", false), "Use HTTP instead of HTTPS")
 	flag.BoolVar(&Config.InsecureHTTPS, "insecurehttps", getEnvOrDefaultBool("XMCINSECURE", false), "Do not validate HTTPS certificates")
 	flag.StringVar(&Config.XMCClientID, "clientid", getEnvOrDefaultString("XMCCLIENTID", ""), "Client ID for OAuth2")
 	flag.StringVar(&Config.XMCClientSecret, "clientsecret", getEnvOrDefaultString("XMCCLIENTSECRET", ""), "Client Secret for OAuth2")
@@ -274,6 +337,7 @@ func parseCLIOptions() {
 		fmt.Fprintf(os.Stderr, "All options that take a value can be set via environment variables:\n")
 		fmt.Fprintf(os.Stderr, "  XMCHOST          -->  -host\n")
 		fmt.Fprintf(os.Stderr, "  XMCPORT          -->  -port\n")
+		fmt.Fprintf(os.Stderr, "  XMCNOHTTPS       -->  -nohttps\n")
 		fmt.Fprintf(os.Stderr, "  XMCINSECURE      -->  -insecurehttps\n")
 		fmt.Fprintf(os.Stderr, "  XMCTIMEOUT       -->  -httptimeout\n")
 		fmt.Fprintf(os.Stderr, "  XMCCLIENTID      -->  -clientid\n")
@@ -284,8 +348,15 @@ func parseCLIOptions() {
 		os.Exit(errUsage)
 	}
 	flag.Parse()
+
+	if Config.NoHTTPS {
+		Config.accessScheme = "http"
+	} else {
+		Config.accessScheme = "https"
+	}
 }
 
+// main ties everything together.
 func main() {
 	// Parse all valid CLI options into variables.
 	parseCLIOptions()
@@ -314,12 +385,13 @@ func main() {
 	// Try to get an OAuth token if we have OAuth authentication data.
 	Config.UseOAuth = false
 	if Config.XMCClientID != "" && Config.XMCClientSecret != "" {
-		_, _, oAuthErr := retrieveOAuthToken()
+		tokenData, oAuthErr := retrieveOAuthToken()
 		if oAuthErr != nil {
 			fmt.Fprintf(os.Stderr, "Could not retrieve OAuth token: %s\n", oAuthErr)
 			os.Exit(errHTTPOAuth)
 		}
-		tokenElements, decodeErr := decodeOAuthToken()
+		OAuth = tokenData
+		tokenElements, decodeErr := decodeOAuthToken(OAuth.AccessToken)
 		if decodeErr != nil {
 			fmt.Fprintf(os.Stderr, "Could not decode OAuth token: %s\n", decodeErr)
 			os.Exit(errHTTPOAuth)
